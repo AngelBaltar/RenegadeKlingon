a82e296006769f61e0c2fc13d0c73f7bd81a95dc
angel <angel.baltar@udc.es>
2013-06-16 12:16:56 +0200

    Deploy system changed Issue Number:
    Description:
        New features:
    
    Benchmarks:
        Developed with the following benchmarks:
    
    Todo:

--- auto_deploy.sh
+++ auto_deploy.sh
@@ -4,4 +4,4 @@
 #lets deploy a .love fot LINUX
 rm ./deployments/linux/RegenageKlingon.love
 zip  RenegadeKlingon.zip -r .
-mv RenegadeKlingon.zip ./deployments/linux/RegenageKlingon.love
+mv RenegadeKlingon.zip ../RenegadeKlingonDeploy/linux/RegenageKlingon.love
