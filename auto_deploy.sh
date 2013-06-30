40fe891ccc110df4b901eac19507b14c600255ff
angel <angel.baltar@udc.es>
2013-06-30 20:59:01 +0200

    New deploy for MAC Issue Number:
    Description:
        New features:
    
    Benchmarks:
        Developed with the following benchmarks:
    
    Todo:
       Test it

--- auto_deploy.sh
+++ auto_deploy.sh
@@ -2,17 +2,31 @@
 #deploy the game to binary executables in deployments directory
 
 necesary_files="GameFrameWork GameScreens Resources Utils main.lua conf.lua"
+path_linux=../RenegadeKlingonDeploy/linux
+path_windows=../RenegadeKlingonDeploy/windows
+path_mac=../RenegadeKlingonDeploy/mac
+path_act=`pwd`
 
 
 #lets deploy a .love for LINUX
-rm ../RenegadeKlingonDeploy/linux/*
-zip  RenegadeKlingon.zip -r $necesary_files
-mv RenegadeKlingon.zip ../RenegadeKlingonDeploy/linux/RenegadeKlingon.love
+rm $path_linux/*
+zip  $path_linux/RenegadeKlingon.zip -r $necesary_files
+mv $path_linux/RenegadeKlingon.zip $path_linux/RenegadeKlingon.love
 
 #lets deploy a .exe for WINDOWS
-rm ../RenegadeKlingonDeploy/windows/*
-cp ../RenegadeKlingonDeploy/linux/RenegadeKlingon.love ../RenegadeKlingonDeploy/windows/game.love
-cp -R ../RenegadeKlingonDeploy/love-0.8.0-win-x86/* ../RenegadeKlingonDeploy/windows/
-cat ../RenegadeKlingonDeploy/windows/love.exe ../RenegadeKlingonDeploy/windows/game.love > ../RenegadeKlingonDeploy/windows/RenegadeKlingon.exe
-rm ../RenegadeKlingonDeploy/windows/*.love
-rm ../RenegadeKlingonDeploy/windows/love.exe
+rm $path_windows/*
+cp $path_linux/RenegadeKlingon.love $path_windows/game.love
+cp -R ../RenegadeKlingonDeploy/love-0.8.0-win-x86/* $path_windows/
+cat $path_windows/love.exe $path_windows/game.love > $path_windows/RenegadeKlingon.exe
+rm $path_windows/*.love $path_windows/love.exe
+
+#lets deploy a .app for MAC OSX
+rm -rf $path_mac/*
+cp -R ../RenegadeKlingonDeploy/love.app $path_mac/RenegadeKlingon.app
+cp $path_linux/RenegadeKlingon.love $path_mac/RenegadeKlingon.app/Contents/Resources/
+sed -i 's/>org.love2d.love</>com.with2balls.RenegadeKlingon</g' $path_mac/RenegadeKlingon.app/Contents/Info.plist
+sed -i 's/>LÖVE</>RENEGADEKLINGON</g' $path_mac/RenegadeKlingon.app/Contents/Info.plist
+cd $path_mac
+zip RenegadeKlingon.osx.zip -r ./RenegadeKlingon.app
+cd $path_act
+rm -rf $path_mac/RenegadeKlingon.app
