07f2de1365097e904cd1fe888cb017463053c9c2
angel <angel.baltar@udc.es>
2013-08-18 13:46:47 +0200

    Changes on deploy system and script Issue Number:
    Description:
        New features:
        Deploy system based in a branch per platform
    
    Benchmarks:
        Developed with the following benchmarks:
    
    Todo:

--- auto_deploy.sh
+++ auto_deploy.sh
@@ -20,22 +20,40 @@ path_act=`pwd`
 
 #lets deploy a .love for LINUX
 echo "deploying for linux..."
+test cd ../RenegadeKlingonDeploy
+test git checkout linux
+test cd ../RenegadeKlingon
 rm $path_linux/*
 test zip  $path_linux/RenegadeKlingon.zip -r $necesary_files
 test mv $path_linux/RenegadeKlingon.zip $path_linux/RenegadeKlingon.love
+test cd ../RenegadeKlingonDeploy
+test git commit -a --message="auto_deploy script deploying linux"
+test git push
+test cd ../RenegadeKlingon
+
 echo "deploy for linux OK"
 
-#lets deploy a .exe for WINDOWS
+# # lets deploy a .exe for WINDOWS
 echo "deploying for windows..."
+test cd ../RenegadeKlingonDeploy
+test git checkout windows
+test cd ../RenegadeKlingon
 rm $path_windows/*
 test cp $path_linux/RenegadeKlingon.love $path_windows/game.love
 test cp -R ../RenegadeKlingonDeploy/love-0.8.0-win-x86/* $path_windows/
 test cat $path_windows/love.exe $path_windows/game.love > $path_windows/RenegadeKlingon.exe
 test rm $path_windows/*.love $path_windows/love.exe
+test cd ../RenegadeKlingonDeploy
+test git commit -a --message="auto_deploy script deploying windows"
+test git push
+test cd ../RenegadeKlingon
 echo "deploy for windows OK"
 
-#lets deploy a .app for MAC OSX
+# #lets deploy a .app for MAC OSX
 echo "deploying for mac..."
+test cd ../RenegadeKlingonDeploy
+test git checkout mac
+test cd ../RenegadeKlingon
 rm -rf $path_mac/*
 test cp -R ../RenegadeKlingonDeploy/love.app $path_mac/RenegadeKlingon.app
 test cp $path_linux/RenegadeKlingon.love $path_mac/RenegadeKlingon.app/Contents/Resources/
@@ -45,4 +63,16 @@ test cd $path_mac
 test zip RenegadeKlingon.osx.zip -r ./RenegadeKlingon.app
 test cd $path_act
 test rm -rf $path_mac/RenegadeKlingon.app
+test cd ../RenegadeKlingonDeploy
+test git commit -a --message="auto_deploy script deploying mac"
+test git push
+test cd ../RenegadeKlingon
 echo "deploy for mac OK"
+
+test cd ../RenegadeKlingonDeploy
+test git checkout master
+test git merge linux
+test git merge windows
+test git merge mac
+test git push
+test cd ../RenegadeKlingon
