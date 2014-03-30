ba72010772c0f038cb6dfcd62333f19f98c2e6bb
angel <angel.baltar@udc.es>
2014-03-30 13:35:45 +0200

    Adding license info to every file Issue Number:
    Description:
        New features:
    
    Benchmarks:
        Developed with the following benchmarks:
    
    Todo:

--- GameScreens/GameScreen.lua
+++ GameScreens/GameScreen.lua
@@ -1,3 +1,21 @@
+-- /* RenegadeKlingon - LÖVE2D GAME
+--  * GameScreen.lua
+--  * Copyright (C) Angel Baltar Diaz
+--  *
+--  * This program is free software: you can redistribute it and/or
+--  * modify it under the terms of the GNU General Public
+--  * License as published by the Free Software Foundation; either
+--  * version 3 of the License, or (at your option) any later version.
+--  *
+--  * This program is distributed in the hope that it will be useful,
+--  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+--  * General Public License for more details.
+--  *
+--  * You should have received a copy of the GNU General Public
+--  * License along with this program.  If not, see
+--  * <http://www.gnu.org/licenses/>.
+--  */
 require 'GameFrameWork/Hud'
 require 'GameFrameWork/Level'
 require 'Utils/Debugging'
@@ -20,7 +38,7 @@ function GameScreen:initialize(autoplay)
     self._space=Space:new()
     self._levels={}
 
-    self._levels[0]="map3.tmx"
+    self._levels[0]="map1.tmx"
     self._levels[1]="map2.tmx"
     self._levels[2]="map3.tmx"
 
