8531454f468bf3216a60c59e76c7352ddec465f5
angel <angel.baltar@udc.es>
2014-04-13 21:58:37 +0200

    fixing game maps Issue Number:
    Description:
        New features:
    
    Benchmarks:
        Developed with the following benchmarks:
    
    Todo:

--- GameScreens/GameScreen.lua
+++ GameScreens/GameScreen.lua
@@ -38,7 +38,7 @@ function GameScreen:initialize(autoplay)
     self._space=Space:new()
     self._levels={}
 
-    self._levels[0]="map2.tmx"
+    self._levels[0]="map1.tmx"
     self._levels[1]="map2.tmx"
     self._levels[2]="map3.tmx"
 
